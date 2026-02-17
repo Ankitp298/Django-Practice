cc085ec19cc779a8ae3f8c9402ff3852ee5ddc64
Ankitp298 <panchalankit298@gmail.com>
2026-02-17 19:01:35 +0530

    Views-and-TemplateView

--- cbv/urls.py
+++ cbv/urls.py
@@ -1,10 +1,28 @@
 from django.urls import path
 from cbv import views
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('',views.home), 
     path('home/',views.home2), 
     path('fun2/',views.fun2), 
     # path('newfun/',views.newfun),
-    path('newfun2/',views.newfun,{"template_name" : "cbv/newfun2.html"})
+    path('newfun2/',views.newfun,{"template_name" : "cbv/newfun2.html"}),
+
+    path('cbv/',views.Home.as_view(),name='home'),
+    path("cbv2/", views.Home2.as_view(), name="home2"),
+    # path("mycbv/", views.Myclasshome.as_view(), name="myclasshome"),
+    path("mycbv/", views.Myclasshome.as_view(name = 'Panchal'), name="myclasshome"),
+    path("chmycbv/", views.ChildClass.as_view(), name="childclass"),
+
+    path("clfun/", views.clfun.as_view(), name="clfun"),
+    path("clfun2/", views.clfun2.as_view(template_name="cbv/newfun2.html"), name="clfun2"),
+
+
+    # Template Views in CBV
+
+    path("temp-home/", TemplateView.as_view(template_name= "cbv/tempview/home.html"), name="temp-home"),
+    # path('temp-home2/',views.TempHome2.as_view(),name='temp-home2'),
+    path('temp-home2/<int:id>',views.TempHome2.as_view(),name='temp-home2'),
+
 ]
--- cbv/views.py
+++ cbv/views.py
@@ -1,23 +1,70 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import View
 
 # Create your views here.
 def home(request):
     return HttpResponse("this is Homepage")
 
+class Home(View):
+    def get(self,request):
+        return HttpResponse('This is Class Base View !!! ')
+
 def home2(request):
     return HttpResponse("<h1> this is Homepage </h1>")
+
+class Home2(View):
+    def get(self,request):
+        return HttpResponse("<h1> this is Homepage </h1>")
+
+class Myclasshome(View):
+    name = "Ankit"
+    def get(self,request):
+        return HttpResponse(f"Name : {self.name}")
+    
+class ChildClass(Myclasshome):
+    def get(self, request):
+        return HttpResponse(f"From Child Class Name : {self.name}")
 
 def fun2(request):
     context = {'msg': "this is context from Function base view!!"}
     return render(request,'cbv/fun2.html',context)
+
+class clfun(View):
+    def get(self,request):
+        context = {'msg': "this is context from Class base view!!"}
+        return render(request,'cbv/fun2.html',context)
+        
 
 def newfun(request,template_name):
     template_name = template_name
     context = {'newmsg': "this is New Funcation context from Function base view!!"}
     return render(request,template_name,context)
 
+class clfun2(View):
+    template_name = ''
+    context = {'newmsg': "this is New Class context from Class base view!!"}
+    def get(self,request):
+        return render(request,self.template_name,context=self.context)
+    
 # def newfun(request):
 #     template_name = "cbv/newfun.html"
 #     context = {'newmsg': "this is context from Function base view!!"}
 #     return render(request,template_name,context)
+
+
+# ========================================================================================================================
+#Template View in Django
+
+from django.views.generic import TemplateView
+
+class TempHome2(TemplateView):
+    template_name = 'cbv/tempview/home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['name'] = "Ankit"
+        context['roll'] = 123
+        print(context)
+        print(kwargs)
+        return context
